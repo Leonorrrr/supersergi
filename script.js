6fad73318376ef02fac01d1bf8c84e714e252e00
Leonorrrr <llucero21213@iesjoanramis.org>
2026-05-15 08:45:18 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,60 +3,107 @@ document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
 
     let gravity = 0.9
+    let estaBotant = false;
 
-    function control(tecla){
+    function control(tecla) {
 
-        if(tecla.code == 'Space'){
-           jump();
+        if (tecla.code == 'Space') {
+            if (estaBotant == false) {
+                jump();
+            }
         }
-        if(tecla.key == 'c'){
-           transformar();
+        if (tecla.key == 'c') {
+            transformar();
         }
-        if(tecla.key == 'j'){
-           volver();
+        if (tecla.key == 'j') {
+            volver();
         }
     }
 
     let position = 0;
-    function jump(){
+    function jump() {
+        estaBotant = true;
         let count = 0;
-       let puja = setInterval(function(){
-        if(count == 15){
-            clearInterval(puja)
-            let baixa = setInterval(function(){
-                 if(count == 0){
-            clearInterval(baixa)}
-                position -= 5;
-                count--;
-                position = position*gravity;
-                sergi.style.bottom = position+ 'px';
-            },20)
-        }
-        position += 50
-        position = position*gravity
-        count++
-        sergi.style.bottom = position+ 'px'
-       },20) 
+        let puja = setInterval(function () {
+            if (count == 15) {
+                clearInterval(puja)
+                let baixa = setInterval(function () {
+                    if (count == 0) {
+                        clearInterval(baixa)
+                        estaBotant = false;
+
+                    }
+                    position -= 5;
+                    count--;
+                    position = position * gravity;
+                    sergi.style.bottom = position + 'px';
+                }, 20)
+            }
+            position += 50
+            position = position * gravity
+            count++
+            sergi.style.bottom = position + 'px'
+        }, 20)
     }
-    
-    function transformar(){
+
+    function transformar() {
         sergi.style.backgroundImage = "url('./img/batman.png')";
-     
+
     }
 
-    function volver(){
+    function volver() {
         sergi.style.backgroundImage = "url('./img/spiderman.png')";
-     
+
     }
 
-    function obstaculo(){
-      const obj = document.createElement('div')
-      obj.classList.add('estrellamuerte')
-      grid.appendChild(obj)
+    function obstaculo() {
+
+        let posicio = 1600;
+        const obj = document.createElement('div')
+        obj.classList.add('estrellamuerte')
+
+        obj.style.left = posicio + 'px'
+        grid.appendChild(obj)
+
+        let mueve = setInterval(function () {
+            if (posicio < 0) {
+                clearInterval(mueve);
+                while(grid.firstChild){
+                    grid.removeChild(grid.lastChild)
+
+                }
+            }
+            posicio -= 5;
+            obj.style.left = posicio+ 'px';
+        }, 20)
+
+    }
+
+     function comida() {
+
+        let posicio = 1600;
+        const obj = document.createElement('div')
+        obj.classList.add('comida')
+
+        obj.style.left = posicio + 'px'
+        grid.appendChild(obj)
+
+        let mueve = setInterval(function () {
+            if (posicio < 0) {
+                clearInterval(mueve);
+                while(grid.firstChild){
+                    grid.removeChild(grid.lastChild)
+                    
+                }
+            }
+            posicio -= 5;
+            obj.style.left = posicio+ 'px';
+        }, 20)
 
     }
 
     obstaculo();
+    comida();
 
     document.addEventListener('keydown', control)
 })
